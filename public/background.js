6a13ebbda81471d1815d4631295f0f9467ac5bc1
David Garcia <david.garcia478@gmail.com>
2019-11-17 17:10:58 -0500

    [Fix] Always add listener when background.js is loaded
    Was only doing this on install.

--- public/background.js
+++ public/background.js
@@ -1,97 +1,88 @@
-/*global chrome*/
 /*global screen*/
 /*global navigator*/
 
 var recorder = null;
 
 class ScreenRecorder {
-    constructor(mediaStream) {
-        this._chunks = [];
-        this._recording = false;
-        this._mediaStream = mediaStream;
-        this._mediaRecorder = new MediaRecorder(mediaStream);
-        this._mediaRecorder.ondataavailable = (e) => this.ondataavailable(e);
-        this._mediaRecorder.onstop = (e) => this.onstop(e);
-        this._mediaRecorder.onerror = (e) => this.onerror(e);
-    }
+  constructor(mediaStream) {
+    this._chunks = [];
+    this._recording = false;
+    this._mediaStream = mediaStream;
+    this._mediaRecorder = new MediaRecorder(mediaStream);
+    this._mediaRecorder.ondataavailable = (e) => this.ondataavailable(e);
+    this._mediaRecorder.onstop = (e) => this.onstop(e);
+    this._mediaRecorder.onerror = (e) => this.onerror(e);
+  }
 
-    start() {
-        this._mediaRecorder.start();
-        this._recording = true;
-    }
+  start() {
+    this._mediaRecorder.start();
+    this._recording = true;
+  }
 
-    stop() {
-        this._mediaRecorder.stop();
-        this._mediaStream.getTracks().forEach(function(track) {
-            track.stop();
-        });
-        this._recording = false;
-    }
+  stop() {
+    this._mediaRecorder.stop();
+    this._mediaStream.getTracks().forEach(function(track) { track.stop(); });
+    this._recording = false;
+  }
 
-    get recording() {
-        return this._recording;
-    }
+  get recording() { return this._recording; }
 
-    ondataavailable(e) {
-        this._chunks.push(e.data);
-    }
+  ondataavailable(e) { this._chunks.push(e.data); }
 
-    onerror(e) {
-        console.error("Media recorder error", e.error);
-        this._recording = false;
-    }
+  onerror(e) {
+    console.error("Media recorder error", e.error);
+    this._recording = false;
+  }
 
-    onstop(e) {
-        let blob = new Blob(this._chunks, { 'type' : 'video/x-matroska;codecs=avc1' });
-        let blobUrl = window.URL.createObjectURL(blob);
-        var a = document.createElement("a");
-        a.href = blobUrl;
-        a.download = "recording.mkv";
-        a.click();
-    }
+  onstop(e) {
+    let blob =
+        new Blob(this._chunks, {'type' : 'video/x-matroska;codecs=avc1'});
+    let blobUrl = window.URL.createObjectURL(blob);
+    var a = document.createElement("a");
+    a.href = blobUrl;
+    a.download = "recording.mkv";
+    a.click();
+  }
 }
 
-chrome.runtime.onInstalled.addListener(function() {
-    chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-        if (message.action == "captureScreen") {
-            recorder = null;
-            captureScreen(sendResponse);
-        } else if (message.action == "stopCapture") {
-            stopCapture(sendResponse);
-        } else if (message.action == "getState") {
-            let recording = false;
-            if (recorder !== null) {
-                recording = recorder.recording;
-            }
-            sendResponse({success: true, recording: recording});
-        }
-        // return true: Callback will be async, won't be invalidated when this function returns
-        return true;
-    });
+chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+  if (message.action == "captureScreen") {
+    recorder = null;
+    captureScreen(sendResponse);
+  } else if (message.action == "stopCapture") {
+    stopCapture(sendResponse);
+  } else if (message.action == "getState") {
+    let recording = false;
+    if (recorder !== null) {
+      recording = recorder.recording;
+    }
+    sendResponse({success : true, recording : recording});
+  }
+  // return true: Callback will be async, won't be invalidated when this
+  // function returns
+  return true;
 });
 
 function captureScreen(callback) {
-    // This must exist in the extension scope (not content script) since we don't want this
-    // page to be created solely in the size of the content script scope.
+  // This must exist in the extension scope (not content script) since we don't
+  // want this
+  // page to be created solely in the size of the content script scope.
 
-    let promise = navigator.mediaDevices.getDisplayMedia({
-        video: {
-            width: screen.width,
-            height: screen.height,
-            framerate: 60
-        }
-    });
-    promise.then((mediaStream) => {
+  let promise = navigator.mediaDevices.getDisplayMedia(
+      {video : {width : screen.width, height : screen.height, framerate : 60}});
+  promise
+      .then((mediaStream) => {
         recorder = new ScreenRecorder(mediaStream);
         recorder.start();
-        callback({success: true});
-    }).catch((error) => {
+        callback({success : true});
+      })
+      .catch((error) => {
         console.error("Failed to create stream: ", error.message);
-        callback({success: false});
-    });
+        callback({success : false});
+      });
 }
 
 function stopCapture(callback) {
-    recorder.stop();
-    callback({success: true});
+  recorder.stop();
+  callback({success : true});
 }
--- src/App.js
+++ src/App.js
@@ -4,64 +4,59 @@ import React from 'react';
 import './App.css';
 
 class ScreenRecordButton extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {recording: false};
-        chrome.runtime.sendMessage({action: "getState"},
-            function(result) {
-                if (result && result.success) {
-                    this.setState({recording: result.recording});
-                }
-            }.bind(this)
-        );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {recording : false};
+    chrome.runtime.sendMessage({action : "getState"}, function(result) {
+      if (result && result.success) {
+        this.setState({recording : result.recording});
+      }
+    }.bind(this));
+  }
 
-    render() {
-        if (!this.state.recording) {
-            return(<button onClick={() => this.recordScreen()}>Record</button>);
+  render() {
+    if (!this.state.recording) {
+      return (<button onClick = {() => this.recordScreen()}>Record<
+              /button>);
         } else {
             return(<button onClick={() => this.stopRecording()}>Stop Recording</button>);
-        }
     }
+  }
 
-    recordScreen() {
-        chrome.runtime.sendMessage({action: "captureScreen"},
-            function(result) {
-                var lastError = chrome.runtime.lastError;
-                if (lastError) {
-                    console.log(lastError.message);
-                    // 'Could not establish connection. Receiving end does not exist.'
-                    return;
-                }
+  recordScreen() {
+    chrome.runtime.sendMessage({action : "captureScreen"}, function(result) {
+      var lastError = chrome.runtime.lastError;
+      if (lastError) {
+        console.log(lastError.message);
+        // 'Could not establish connection. Receiving end does not exist.'
+        return;
+      }
 
-                if (result && result.success) {
-                    console.log("We are now recording");
-                    this.setState({recording: true});
-                } else {
-                    console.error("Failed to start stream");
-                }
-            }.bind(this)
-        );
-    }
+      if (result && result.success) {
+        console.log("We are now recording");
+        this.setState({recording : true});
+      } else {
+        console.error("Failed to start stream");
+      }
+    }.bind(this));
+  }
 
-    stopRecording() {
-        chrome.runtime.sendMessage({action: "stopCapture"},
-            function(result) {
-                if (result && result.success) {
-                    console.log("Stopped recording");
-                    this.setState({
-                        recording: false,
-                    });
-                } else {
-                    console.error("Failed to stop capturing");
-                }
-            }.bind(this)
-        );
-    }
+  stopRecording() {
+    chrome.runtime.sendMessage({action : "stopCapture"}, function(result) {
+      if (result && result.success) {
+        console.log("Stopped recording");
+        this.setState({
+          recording : false,
+        });
+      } else {
+        console.error("Failed to stop capturing");
+      }
+    }.bind(this));
+  }
 }
 
 class App extends React.Component {
-    render() {
+  render() {
         return (
         <div className="App">
           <header className="App-header">
